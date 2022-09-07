d64412210a577a8118d056a350f15e593dc13b39
Hardik Bhaavani <bhaavanihardik@outlook.com>
2022-09-07 08:47:05 +0530

    Added Home Page Base Code and Material UI Library

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Header } from './common/header/Header';
+import Home from './screens/home/Home'
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
+    <Home />
   </React.StrictMode>
 );
---
+++ src/screens/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Home.css'
+import Header from '../../common/header/Header'
+
+export const Home = () => {
+    return (
+        <div><Header /></div>
+    )
+}
+
+export default Home;
