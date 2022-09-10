e6d95d32ff3565bdbe74e776bb0841385390f52d
Hardik Bhaavani <bhaavanihardik@outlook.com>
2022-09-10 09:54:37 +0530

    Horizontal Scroll Bar added

--- src/common/GridList.js
+++ src/common/GridList.js
@@ -3,13 +3,18 @@ import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 
-export default function TitlebarImageList({data}) {
+export default function TitlebarImageList({ data }) {
     return (
-        <ImageList sx={{ height: 250 }} cols={5} >
+        <ImageList sx={{
+            height:250,
+            gridAutoFlow: "column",
+            gridTemplateColumns: "repeat(auto-fill,minmax(250px,1fr)) !important",
+            gridAutoColumns: "minmax(250px, 1fr)"
+        }} >
             {data.map((item) => (
                 <ImageListItem key={item.id} >
                     <img
-                        src={`${item.poster_url}?w=248&fit=crop&auto=format`}
+                        src={item.poster_url}
                         alt={item.title}
                         loading="lazy"
                     />
