0ac441a6dcffb985e22bc4f2047a1a70921780f4
Hardik Bhaavani <bhaavanihardik@outlook.com>
2022-09-10 10:26:11 +0530

    Added Released Movie Content

---
+++ src/common/ReleasedMovieContent.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+
+const ReleasedMovieContent = ({ data }) => {
+
+    function releaseDate(date) {
+        let releaseDate = new Date(date);
+        // console.log(releaseDate);
+        return releaseDate.toDateString();
+    }
+
+    return (
+        <ImageList sx={{
+            height:350
+        }} cols={4}>
+            {data.map((item) => (
+                <ImageListItem key={item.id} >
+                    <img
+                        src={item.poster_url}
+                        alt={item.title}
+                        loading="lazy"
+                    />
+                    <ImageListItemBar
+                        title={item.title}
+                        subtitle={`Release Date : ${releaseDate(item.release_date)}`}
+                    />
+                </ImageListItem>
+            ))}
+        </ImageList>
+    );
+}
+
+export default ReleasedMovieContent
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -4,12 +4,23 @@ import Header from '../../common/header/Header'
 import '../../common/GridList'
 import GridList from '../../common/GridList'
 import moviesData from '../../common/moviesData'
+import ReleasedMovieContent from '../../common/ReleasedMovieContent'
 
 export const Home = () => {
     return (
         <div>
-            <Header heading={'Upcoming Movies'}/>
-            <GridList data={moviesData}/>
+            <Header heading={'Upcoming Movies'} />
+            <GridList data={moviesData} />
+            <div className="flex-container">
+                <div className='left'>
+                    {/* Released Movies Left Side Container */}
+                    <ReleasedMovieContent data={moviesData} />
+                </div>
+                <div className='right'>
+                    {/* Filter Container */}
+                    Filters
+                </div>
+            </div>
         </div>
     )
 }
